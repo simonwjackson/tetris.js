21d17875bde134ab134f9898f0b8481bcb0bbc3c
Simon W. Jackson <simon@simonwjackson.com>
2018-08-15 16:22:05 -0500

    refactor hasCollision

--- src/core.js
+++ src/core.js
@@ -1,18 +1,16 @@
-// Refactor: should return true ASAP
 export const hasCollision = (arena, player) => {
   const m = player.matrix
   const o = player.pos
-  let found = false
 
-  m.map((yVal, yIdx) => {
-    yVal.map((xVal, xIdx) => {
+  return m.some((yVal, yIdx) => (
+    yVal.some((xVal, xIdx) => {
       if (m[yIdx][xIdx] !== 0 &&
         (arena[yIdx + o.y] &&
           arena[yIdx + o.y][xIdx + o.x]) !== 0) {
-        found = true;
-      }
-    })
-  })
+        return true
+      } 
 
-  return found;
+      return false
+    })
+  ))
 }
