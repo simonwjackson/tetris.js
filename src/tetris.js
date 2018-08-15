890dcb7590f93b545f6dc7f3e3d3069d58078363
Simon W. Jackson <simon@simonwjackson.com>
2018-08-15 00:22:46 -0500

    add offset

--- src/tetris.js
+++ src/tetris.js
@@ -5,13 +5,14 @@ if (module.hot) {
         throw 'whatever'
     })
 }
- 
+
+import './hotfix' 
 import './main.css'
 
 const canvas = document.getElementById('tetris')
 const context = canvas.getContext('2d')
 
-context.scale(20, 20)
+context.scale(20, 20) 
 context.fillStyle = '#000'
 context.fillRect(0, 0, canvas.width, canvas.height)
 
@@ -21,15 +22,15 @@ const matrix = [
   [0, 1, 0]
 ]
 
-const drawMatrix = () => { 
+const drawMatrix = (matrix, offset) => { 
   matrix.map((row, y) => {
     row.map((value, x) => {
       if (value !== 0) {
         context.fillStyle = 'hsl(226, 96%, 56%)'
-        context.fillRect(x, y, 1, 1)
+        context.fillRect(x + offset.x, y + offset.y, 1, 1)
       }
     })
   })
 }
 
-drawMatrix()
+drawMatrix(matrix, { x: 5, y: 5 })
